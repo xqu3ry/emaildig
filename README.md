d9ad8deada9ded64cd675f31f20178facf4bb91b
xqu3ry <157222186+xqu3ry@users.noreply.github.com>
2025-05-11 15:53:21 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# emaildig
-Email information digging tool
+
+***emaildig*** - email information digging tool. Get info from leaks, reputation, history and etc.
+
+Tool can be useful without any API keys, but for greater efficiency you can enter your own API keys for different services
+
+**Download emaildig:**
+
+```
+git clone https://github.com/xqu3ry/emaildig
+```
+
+```
+cd emaildig
+```
+
+**Usage:**
+
+```
+python3 emaildig.py -T example@example.com
+```
+
+Or
+
+```
+python emaildig.py -T example@example.com
+```
+
+Edit your config in '*config.ini*' file
+
+Provide your API keys to '*modules/api_keys.py*' file
+
+Edit the entries limit you want to see for each service in '*modules/limits.py*' file
+
+**Available  Services:**
+
+
+| Hunter              | https://hunter.io/                    | 🔑     |
+| ------------------- | ------------------------------------- | ------ |
+| **Emailrep**        | https://emailrep.io/                  | 🔑     |
+| **IpQualityScore**  | https://www.ipqualityscore.com/       | 🔑     |
+| **ProxyNova**       | https://www.proxynova.com/tools/comb/ | ✅      |
+| **DeHashed**        | https://dehashed.com/                 | 🔑     |
+| **LeakLookup**      | https://leak-lookup.com/              | 🔑     |
+| **Leakcheck**       | https://leakcheck.io/                 | ✅ - 🔑 |
+| **Snusbase**        | https://snusbase.com                  | 🔑     |
+| **DataBreach**      | https://databreach.com/               | 🔑     |
+| **HIBP**            | https://haveibeenpwned.com/           | 🔑     |
+| **BreachDirectory** | https://breachdirectory.org           | ✅      |
+
+
---
+++ emaildig.py
@@ -0,0 +1,104 @@
+import ascii_text
+import sys
+import configparser
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
+
+config = configparser.ConfigParser()
+
+config.read('config.ini')
+
+import api_keys
+
+from modules import proxynova,hunter,ipqualityscore,leaklookup,breachdirectory,haveibeenpwned,snusbase,leakcheck,databreach,dehashed,emailrep
+
+import argparse
+
+parser = argparse.ArgumentParser(description="EmailDig — email digging tool")
+
+parser.add_argument("-T", "--target", required=True, help="Email to search")
+
+args = parser.parse_args()
+
+email = args.target
+
+if config.getboolean('SETTINGS', 'ascii_art'):
+	
+	print(ascii_text.art)
+
+if config.getboolean('SETTINGS', 'version'):
+
+	print("version 0.1")
+
+if config.getboolean('SERVICES', 'use_proxynova'):
+
+	print("\nProxyNova Results:")
+
+	proxynova.proxynova(email)
+
+if config.getboolean('SERVICES', 'use_breachdirectory'):
+
+	print("\nBreachDirectory Results:")
+
+	breachdirectory.breachdirectory(email)
+
+if config.getboolean('SERVICES', 'use_hunter'):
+
+	print("\nHunter Results:")
+
+	hunter.hunter(email)
+
+if config.getboolean('SERVICES', 'use_ipqualityscore'):
+
+	print("\nIpQualityScore Results:")
+
+	ipqualityscore.ipqualityscore(email)
+
+if config.getboolean('SERVICES', 'use_emailrep'):
+
+	print("\nEmailrep Results:")
+
+	emailrep.emailrep(email)
+
+if config.getboolean('SERVICES', 'use_haveibeenpwned'):
+
+	print("\nHaveIBeenPwned Results:")
+
+	haveibeenpwned.haveibeenpwned(email)
+
+if config.getboolean('SERVICES', 'use_snusbase'):
+
+	print("\nSnusbase Results:")
+
+	snusbase.snusbase(email)
+
+if config.getboolean('SERVICES', 'use_leaklookup'):
+
+	print("\nLeaklookup Results:")
+
+	leaklookup.leaklookup(email)
+
+if config.getboolean('SERVICES', 'use_dehashed'):
+
+	print("\nDeHashed Results:")
+
+	dehashed.dehashed(email)
+
+if config.getboolean('SERVICES', 'use_databreach'):
+
+	print("\nDatabreach Results:")
+
+	databreach.databreach(email)
+
+if config.getboolean('SERVICES', 'use_leakcheck'):
+
+	print("\nLeakcheck Results:")
+
+	leakcheck.leakcheck(email)
+
+
+
+
+
+
